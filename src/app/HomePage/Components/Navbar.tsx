c97bd3384a5de967e462ae9b66f0b0216c0c1aae
Chris <96Schmitzc@gmail.com>
2024-10-30 14:02:28 -0500

    finishing navbar and setting up clerk

--- src/app/HomePage/Components/Navbar.tsx
+++ src/app/HomePage/Components/Navbar.tsx
@@ -1,6 +1,41 @@
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import Link from "next/link";
+
 export function Navbar() {
     return (
-        <div>Hello World</div>
-    )
+        <header className="flex py-6 shadow-xl fixed top-0 w-full z-10 bg-background/95">
+            <nav className="flex items-center justify-between container mx-auto px-6 font-semibold">
+                {/* Logo Section */}
+                <div className="ml-10">
+                    <Link href="#" aria-label="Map Forge Home" className="text-2xl font-bold">
+                        Map Forge
+                    </Link>
+                </div>
+                
+                {/* Navigation Links */}
+                <div className="flex items-center gap-8">
+                    <Link href="#features" className="text-xl" aria-label="Features">
+                        Features
+                    </Link>
+                    <Link href="#pricing" className="text-xl" aria-label="Pricing">
+                        Pricing
+                    </Link>
+                    
+                    {/* Account Links */}
+                    <div className="ml-4">
+                        <SignedIn>
+                            <Link href="/MyMaps" className="text-lg" aria-label="My Maps">
+                                My Maps
+                            </Link>
+                        </SignedIn>
+                        <SignedOut>
+                            <SignInButton>
+                                <button className="text-lg" aria-label="Login">Login</button>
+                            </SignInButton>
+                        </SignedOut>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
 import { Navbar } from "./HomePage/Components/Navbar";
+// import HeroSection from "./HomePage/Components/HeroSection";
+// import FeaturesSection from "./HomePage/Components/FeaturesSection";
+// import PricingSection from "./HomePage/Components/PricingSection";
 
 
 export default function Home() {
   return (
     <div>
-      <section>
+      <section className="py-4 flex ">
         <Navbar />
       </section>
+
+      <main>
+        {/* <HeroSection />
+        <FeaturesSection />
+        <PricingSection /> */}
+      </main>
     </div>
   );
 }
